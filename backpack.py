59c13cf3f33c0d2c7ea66214b58db56c7ff0e10c
mr-eggleton <meggleton@utcsheffield.org.uk>
2014-01-25 18:31:07 +0000

    video splitting and capture

--- backpack.py
+++ backpack.py
@@ -2,27 +2,47 @@ import picamera
 import time
 
 
+outputbasedir =  '~/BPOA/' 
+videocachedir = outputbasedir+'videocache/'
+startuppause = 5
+timelapsedir = '~/timelapse'
+
+def getFileName(curtime, ext):
+  name = time.strftime("%Y-%m-%d-%H-%M-%S", curtime)+"."+ext
+  return name
+  
+def getFolderName(curtime, ext):
+  name = outputbasedir+ext'/'+time.strftime('%Y-%m/%d/%H/', curtime)+'/'
+  return name
+
+  
+
 with picamera.PiCamera() as camera:
 
 
   #Power up
-  #camera.resolution = (1920, 1080) #1080P Full HD 1920x1080
+  camera.resolution = (1920, 1080) #1080P Full HD 1920x1080
   #camera.start_preview()
-  time.sleep(2)
+  time.sleep(startuppause)
   start = time.time()
 
-  # ('img{timestamp:%Y-%m-%d-%H-%M}.jpg'):
   
   #calc name
-  #camera.start_recording('foo.h264')
+  camera.start_recording(videocachedir+getFileName(start,'h264'),  inline_headers=True)
+  camera.wait_recording(startuppause)
   
-  #camera.capture('foo.jpg', use_video_port=True)
-  #camera.wait_recording(10)
-  #camera.stop_recording()
-
+  camera.capture(getFolderName(start+getFileName(start,'jpg'), use_video_port=True)
+  camera.wait_recording(60)
+  
+  now =time.time()
+  camera.split_recording(videocachedir+getFileName(now,'h264'))
+  camera.capture(getFolderName(now+getFileName(start,'jpg'), use_video_port=True)
+  camera.wait_recording(60)
+  
+  
   
   #Start video
-  #Set the next time a photo should be taken
+  #Set the next time a photo should be takne
   #Set the next time the video should change name
 
   #Loop
@@ -42,4 +62,5 @@ with picamera.PiCamera() as camera:
 		  #Check the file system to see if there any videos more than 10 minutes older  than  ten minutes without an event and eith delete or move
 
 
+  camera.stop_recording()
 
