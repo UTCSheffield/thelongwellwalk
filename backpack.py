e453f9940d073e28d1825d0b9aaf9d123d4bdae1
Martyn Eggleton <martyn.eggleton@gmail.com>
2014-01-26 19:40:38 +0000

    recording loop working

--- backpack.py
+++ backpack.py
@@ -8,6 +8,10 @@ videocachedir = outputbasedir+'videocache/'
 
 startuppause = 5
 timelapsedir = os.path.expanduser('~/timelapse')
+debug = True
+
+if (debug):
+  print("backback started")
 
 def getFileName(curtime, ext):
   name = time.strftime("%Y-%m-%d-%H-%M-%S", curtime)+"."+ext
@@ -23,51 +27,64 @@ if( os.path.exists(videocachedir) != True ):
   os.makedirs(videocachedir)
 
 with picamera.PiCamera() as camera:
-
+  
 
   #Power up
-  camera.resolution = (1920, 1080) #1080P Full HD 1920x1080
+  #camera.resolution = (1920, 1080) #1080P Full HD 1920x1080
+  camera.resolution = (1280, 720)
+  camera.framerate = 25
+  
   #camera.start_preview()
   time.sleep(startuppause)
   start = time.gmtime()
 
   
   #calc name
-  camera.start_recording(videocachedir+getFileName(start,'h264'),  inline_headers=True)
-  camera.wait_recording(startuppause)
-  
-  camera.capture(getFolderName(start,'jpg')+getFileName(start,'jpg'), use_video_port=True)
-  camera.wait_recording(50)
-  
-  now =time.gmtime()
-  camera.split_recording(videocachedir+getFileName(now,'h264'))
-  camera.wait_recording(10)
-  
-  camera.capture(getFolderName(now,'jpg')+getFileName(now,'jpg'), use_video_port=True)
-  camera.wait_recording(60)
-  
-  
-  
-  #Start video
-  #Set the next time a photo should be takne
-  #Set the next time the video should change name
+  videoname = videocachedir+getFileName(start,'h264')
+  camera.start_recording(videoname,  inline_headers=True)
+  if (debug):
+    print("video started "+videoname)
 
-  #Loop
-  #while keyboard test.
-    #camera.wait_recording(10)
   
-    #Check the buttons
+  while True:
+    camera.wait_recording(10)
+    stillnow = time.gmtime()
+    stillname = getFolderName(stillnow,'jpg')+getFileName(stillnow,'jpg')
     
-	  #Save any button presses in the file system
-    #Check the time : If  photo should be taken 
-	    #Generate file name photos/2014-01/15/17/56-59
-      #Take
+    camera.capture(stillname, use_video_port=True)
+    if (debug):
+      print("still "+stillname)
+    
+    camera.wait_recording(50)
+    
+    if (debug):
+      print("video ending "+videoname)
 
-	  #Check the time : If video should change name
-		  #Generate file name videobuffer/2014-01-15-17-56-59
-		  #Change video file
-		  #Check the file system to see if there any videos more than 10 minutes older  than  ten minutes without an event and eith delete or move
+    now = time.gmtime()
+    videoname = videocachedir+getFileName(now,'h264')  
+    camera.split_recording(videoname)
+    if (debug):
+      print("video started "+videoname)
 
-
+    
+    
+    #Loop
+    #while keyboard test.
+      #camera.wait_recording(10)
+    
+      #Check the buttons
+      
+      #Save any button presses in the file system
+      #Check the time : If  photo should be taken 
+        #Generate file name photos/2014-01/15/17/56-59
+        #Take
+  
+      #Check the time : If video should change name
+        #Generate file name videobuffer/2014-01-15-17-56-59
+        #Change video file
+        #Check the file system to see if there any videos more than 10 minutes older  than  ten minutes without an event and eith delete or move
+  
+  
+  
   camera.stop_recording()
 
