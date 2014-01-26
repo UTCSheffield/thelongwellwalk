ba754ca574279017ca718367b95460bd1ecdb125
Martyn Eggleton <martyn.eggleton@gmail.com>
2014-01-26 06:57:44 +0000

    runs but video files don't playback properly

--- backpack.py
+++ backpack.py
@@ -1,21 +1,26 @@
 import picamera
 import time
+import os
 
 
-outputbasedir =  '~/BPOA/' 
+outputbasedir =  os.path.expanduser('~/BPOA/') 
 videocachedir = outputbasedir+'videocache/'
+
 startuppause = 5
-timelapsedir = '~/timelapse'
+timelapsedir = os.path.expanduser('~/timelapse')
 
 def getFileName(curtime, ext):
   name = time.strftime("%Y-%m-%d-%H-%M-%S", curtime)+"."+ext
   return name
   
 def getFolderName(curtime, ext):
-  name = outputbasedir+ext'/'+time.strftime('%Y-%m/%d/%H/', curtime)+'/'
+  name = outputbasedir+ext+'/'+time.strftime('%Y-%m/%d/%H/', curtime)+'/'
+  if( os.path.exists(name) != True ):
+    os.makedirs(name)
   return name
 
-  
+if( os.path.exists(videocachedir) != True ):
+  os.makedirs(videocachedir)
 
 with picamera.PiCamera() as camera:
 
@@ -24,19 +29,21 @@ with picamera.PiCamera() as camera:
   camera.resolution = (1920, 1080) #1080P Full HD 1920x1080
   #camera.start_preview()
   time.sleep(startuppause)
-  start = time.time()
+  start = time.gmtime()
 
   
   #calc name
   camera.start_recording(videocachedir+getFileName(start,'h264'),  inline_headers=True)
   camera.wait_recording(startuppause)
   
-  camera.capture(getFolderName(start+getFileName(start,'jpg'), use_video_port=True)
-  camera.wait_recording(60)
+  camera.capture(getFolderName(start,'jpg')+getFileName(start,'jpg'), use_video_port=True)
+  camera.wait_recording(50)
   
-  now =time.time()
+  now =time.gmtime()
   camera.split_recording(videocachedir+getFileName(now,'h264'))
-  camera.capture(getFolderName(now+getFileName(start,'jpg'), use_video_port=True)
+  camera.wait_recording(10)
+  
+  camera.capture(getFolderName(now,'jpg')+getFileName(now,'jpg'), use_video_port=True)
   camera.wait_recording(60)
   
   
