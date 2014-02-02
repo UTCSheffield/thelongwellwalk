f792d0c7905554b8e3a47a3e168977f77065c57e
Martyn Eggleton <martyn.eggleton@gmail.com>
2014-02-02 20:49:04 +0000

    random instead of button

--- backpack.py
+++ backpack.py
@@ -1,90 +1,113 @@
 import picamera
 import time
 import os
+import io
+import random
+import picamera
+from PIL import Image
 
+prior_image = None
+tl_count = 0
+tl_target = 60
+startuppause = 5
+buffer_length = 15
 
 outputbasedir =  os.path.expanduser('~/BPOA/') 
 videocachedir = outputbasedir+'videocache/'
 
-startuppause = 5
 timelapsedir = os.path.expanduser('~/timelapse')
 debug = True
 
-if (debug):
-  print("backback started")
 
 def getFileName(curtime, ext):
-  name = time.strftime("%Y-%m-%d-%H-%M-%S", curtime)+"."+ext
-  return name
+    name = time.strftime("%Y-%m-%d-%H-%M-%S", curtime)+"."+ext
+    return name
   
 def getFolderName(curtime, ext):
-  name = outputbasedir+ext+'/'+time.strftime('%Y-%m/%d/%H/', curtime)+'/'
-  if( os.path.exists(name) != True ):
-    os.makedirs(name)
-  return name
+    name = outputbasedir+ext+'/'+time.strftime('%Y-%m/%d/%H/', curtime)+'/'
+    if( os.path.exists(name) != True ):
+        os.makedirs(name)
+    return name
+
+def check_button(camera):
+    result = random.randint(0, 20) == 0
+    return result
+
+def dotimelapse():
+    global tl_count, tl_target
+    tl_count ++
+    if (tl_count >= tl_target):
+        tl_count = 0
+        stillnow = time.gmtime()
+        stillname = getFolderName(stillnow,'jpg')+getFileName(stillnow,'jpg')
+        
+        camera.capture(stillname, use_video_port=True)
+        if (debug):
+            print("still "+stillname)
+          
+def write_video(stream):
+    # Write the entire content of the circular buffer to disk. No need to
+    # lock the stream here as we're definitely not writing to it
+    # simultaneously
+    then = time.gmtime(time.time() - buffer_length)
+    videoname = getFolderName(then,'h264')+getFileName(then,'h264')
+    
+    
+    if (debug):
+        print("buffer video writing "+videoname)
+
+    with io.open(videoname, 'wb') as output:
+        for frame in stream.frames:
+            if frame.header:
+                stream.seek(frame.position)
+                break
+        while True:
+            buf = stream.read1()
+            if not buf:
+                break
+            output.write(buf)
+    # Wipe the circular stream once we're done
+    stream.seek(0)
+    stream.truncate()
+    if (debug):
+        print("buffer video ending "+videoname)
+
+
+if (debug):
+    print("backback started")
 
 if( os.path.exists(videocachedir) != True ):
-  os.makedirs(videocachedir)
+    os.makedirs(videocachedir)
 
 with picamera.PiCamera() as camera:
-  
+    camera.resolution = (1920, 1080) #1080P Full HD 1920x1080
+    camera.framerate = 25
+    stream = picamera.PiCameraCircularIO(camera, seconds=buffer_length)
+    camera.start_recording(stream, format='h264')
+    try:
+        while True:
+            camera.wait_recording(1)
+            dotimelapse()
+            if check_button(camera):
+                print('Button Pressed!')
+                
+                now = time.gmtime()
+                videoname = getFolderName(now,'h264')+getFileName(now,'h264')
+                if (debug):
+                    print("button video starting "+videoname)
 
-  #Power up
-  #camera.resolution = (1920, 1080) #1080P Full HD 1920x1080
-  camera.resolution = (1280, 720)
-  camera.framerate = 25
-  
-  #camera.start_preview()
-  time.sleep(startuppause)
-  start = time.gmtime()
+                camera.split_recording(videoname)
+                # Write the buffer before the button press to disk as well
+                write_video(stream)
+                # Wait until motion is no longer detected, then split
+                # recording back to the in-memory circular buffer
+                while check_button(camera):
+                    camera.wait_recording(1)
+                    dotimelapse()
+                print('Button unpressed')
+                camera.split_recording(stream)
+                if (debug):
+                    print("button video ending "+videoname)
 
-  
-  #calc name
-  videoname = videocachedir+getFileName(start,'h264')
-  camera.start_recording(videoname,  inline_headers=True, profile="constrained")
-  if (debug):
-    print("video started "+videoname)
-
-  
-  while True:
-    camera.wait_recording(10)
-    stillnow = time.gmtime()
-    stillname = getFolderName(stillnow,'jpg')+getFileName(stillnow,'jpg')
-    
-    camera.capture(stillname, use_video_port=True)
-    if (debug):
-      print("still "+stillname)
-    
-    camera.wait_recording(10)
-    
-    if (debug):
-      print("video ending "+videoname)
-
-    now = time.gmtime()
-    videoname = videocachedir+getFileName(now,'h264')  
-    camera.split_recording(videoname)
-    if (debug):
-      print("video started "+videoname)
-
-    
-    
-    #Loop
-    #while keyboard test.
-      #camera.wait_recording(10)
-    
-      #Check the buttons
-      
-      #Save any button presses in the file system
-      #Check the time : If  photo should be taken 
-        #Generate file name photos/2014-01/15/17/56-59
-        #Take
-  
-      #Check the time : If video should change name
-        #Generate file name videobuffer/2014-01-15-17-56-59
-        #Change video file
-        #Check the file system to see if there any videos more than 10 minutes older  than  ten minutes without an event and eith delete or move
-  
-  
-  
-  camera.stop_recording()
-
+    finally:
+        camera.stop_recording()
