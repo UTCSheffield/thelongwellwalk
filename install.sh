b724fd575a70d83b78d49b58424642ebcf968063
Martyn Eggleton <martyn.eggleton@gmail.com>
2014-03-20 14:36:44 +0000

    Gets installer going

--- install.sh
+++ install.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 #Installer
 
+sudo apt-get update
+sudo apt-get upgrade
 
-#install picamera 
+#Do GPS install described here
+#http://www.stuffaboutcode.com/2013/09/raspberry-pi-gps-setup-and-python.html
 
-#sudo raspi-config
-#install audio stuff
-#install ddns
+sudo apt-get install gpsd gpsd-clients python-gps ddclient python-picamera python3-picamera rsync unison ppp usb-modeswitch wvdial python-alsaaudio
 
-sudo apt-get install ddclient
-
-
-#install rsync
+mkdir /home/pi/BPOA/
 
 #do ssh-copy-id 
